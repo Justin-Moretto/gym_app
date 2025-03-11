865bcd9ecbf9d334831843f631a8e09c0c5f042d
Justin <justinmorettomtl@gmail.com>
2025-03-10 22:20:40 -0400

    first commit

---
+++ lib/classes.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class ExerciseModel {
+  final String name;
+  int weight;
+  int reps;
+  int sets;
+  // List<MuscleModel> musclesTargeted = [],
+  DateTime? timeLastLogged;
+
+  ExerciseModel({
+    required this.name,
+    this.weight = 0,
+    this.reps = 0,
+    this.sets = 0,
+    // List<MuscleModel> musclesTargeted = [],
+    DateTime? timeLastLogged,
+  });
+}
+
+class MuscleGroupModel {
+  final String name;
+  String imagePath;
+
+  MuscleGroupModel({
+    required this.name,
+    // required List<MuscleModel> muscles,
+    this.imagePath = "assets/absImage.jpg",
+  });
+
+}
+
+class MuscleModel {
+  MuscleModel({required String name, required String muscleGroup});
+}
+
+// Chest & Arms - Pecs, Biceps, Triceps
+// Shoulders & Upper Back - Delts, Lats, Traps
+// Core & Lower Back - Abs, Glutes, Obliques
+// Legs - Quads, Hamstrings, Calves
+
+// Compound Lifts: Squat, bench, Pull-up
---
+++ lib/lifts.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/material.dart';
+
+class LiftsPage extends StatefulWidget {
+  const LiftsPage({super.key});
+
+  @override
+  State<LiftsPage> createState() => _LiftsPageState();
+}
+
+class _LiftsPageState extends State<LiftsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
